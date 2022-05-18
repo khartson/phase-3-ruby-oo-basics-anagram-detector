935b014216d434dd5ebfb59f303c602059686a5c
Kyle <khartson2017@gmail.com>
2022-05-18 18:36:42 -0400

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+# Your code goes here
+class Anagram
+
+    attr_accessor :match
+    def initialize(word)
+        @match_word = word
+    end
+
+    def match(word_arr)
+        match_arr = []
+        for word in word_arr do
+            match_arr.push(word) if word.chars.sort == @match_word.chars.sort
+        end
+        return match_arr
+    end
+        
+end
